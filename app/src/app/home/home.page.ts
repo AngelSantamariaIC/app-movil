16da6d93daa20235587b51d788b13b0b177a93b6
Angel Daniel Santamaria <angel.santamaria@impocali.com>
2025-08-27 21:14:53 -0500

    se organiza forma visual

--- app/src/app/home/home.page.ts
+++ app/src/app/home/home.page.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
+  standalone: false,
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  standalone: false,
 })
 export class HomePage {
-
-  constructor() {}
-
+  mensaje = 'Aún no has hecho clic';
+  clics = 0;
+  saludar() {
+    this.clics++;
+    this.mensaje = `¡Hola! Has hecho clic ${this.clics} vez${this.clics === 1 ? '' : 'es'}.`;
+  }
 }
